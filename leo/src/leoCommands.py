b13d99c3e5328222985a1ba868aa2b4751a4fe34
Edward K Ream <edreamleo@gmail.com>
2006-09-29 01:45:08 +0000

    Preparing for Leo 4.4.2 beta 1

--- leo/src/leoCommands.py
+++ leo/src/leoCommands.py
@@ -289,7 +289,7 @@ class baseCommands:
     #@+node:ekr.20040629121554.1:getSignOnLine (Contains hard-coded version info)
     def getSignOnLine (self):
         c = self
-        return "Leo 4.4.1.1 final, build %s, September 3, 2006" % c.getBuildNumber()
+        return "Leo 4.4.2 beta 1, build %s, September 29, 2006" % c.getBuildNumber()
     #@-node:ekr.20040629121554.1:getSignOnLine (Contains hard-coded version info)
     #@+node:ekr.20040629121554.2:initVersion
     def initVersion (self):
