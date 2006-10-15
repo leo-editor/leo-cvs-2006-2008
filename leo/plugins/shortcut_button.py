a5af7aa9c6c8efa09f9e965a9ea68913bf9c113e
Edward K Ream <edreamleo@gmail.com>
2006-10-15 15:57:11 +0000

    0.3 EKR: Rewritten to use latest mod_scripting plugin.
    As a result, it creates the shortcut-button command, and commands for every button created.

--- leo/plugins/shortcut_button.py
+++ leo/plugins/shortcut_button.py
@@ -3,20 +3,23 @@
 '''A plugin to create a 'Shortcut' button in the icon area.
 
 Pressing the Shortcut button creates *another* button which when pressed will
-select the presently selected node at the time the button was created.'''
+select the presently selected node at the time the button was created.
+
+This plugin requires that the mod_scripting plugin be enabled.'''
 
 #@<< imports >>
 #@+node:ekr.20060601151845.2:<< imports >>
 import leoGlobals as g
 import leoPlugins
-from mod_scripting import scriptingController
+
+import mod_scripting
 
 Tk = g.importExtension('Tkinter',pluginName=__name__,verbose=True)
 #@nonl
 #@-node:ekr.20060601151845.2:<< imports >>
 #@nl
 
-__version__ = "0.2"
+__version__ = "0.3"
 #@<< version history >>
 #@+node:ekr.20060601151845.3:<< version history >>
 #@@nocolor
@@ -24,8 +27,10 @@ __version__ = "0.2"
 # 
 # 0.1 Initial version.  Suggested by Brian Theado.
 # 0.2 EKR: Improved docstring.
+# 0.3 EKR: Rewritten to used latest mod_scripting plugin.
+# As a result, it creates the shortcut-button command, and commands for every 
+# button created.
 #@-at
-#@nonl
 #@-node:ekr.20060601151845.3:<< version history >>
 #@nl
 
@@ -33,7 +38,7 @@ __version__ = "0.2"
 #@+node:ekr.20060601151845.4:init
 def init ():
     
-    ok = Tk and not g.app.unitTesting
+    ok = Tk and mod_scripting and not g.app.unitTesting
     
     if ok:
         if g.app.gui is None:
@@ -58,7 +63,7 @@ def onCreate (tag, keys):
     c = keys.get('c')
 
     if c:
-        sc = scriptingController(c)
+        sc = mod_scripting.scriptingController(c)
         ch = shortcutButton(sc,c)
 #@nonl
 #@-node:ekr.20060601151845.5:onCreate
@@ -69,48 +74,41 @@ class shortcutButton:
     #@+node:ekr.20060601151845.7: ctor
     def __init__ (self,sc,c):
     
-        g.trace()
         self.createShortcutButtonButton(sc,c)
     #@-node:ekr.20060601151845.7: ctor
     #@+node:ekr.20060601153526:createShortcutButtonButton
     def createShortcutButtonButton(self,sc,c):
-    
-        b = sc.createIconButton('Shortcut', 'Create a shortcut button', bg='LightSteelBlue1')
-    
-        def shortcutButtonButtonCallback(self=self,sc=sc,c=c):
+        
+        def shortcutButtonButtonCallback(event=None,self=self,sc=sc,c=c):
             self.createShortcutButton(sc,c)
-            
-        def deleteButtonCallback(event=None,sc=sc,b=b):
-            sc.deleteButton(b)
     
-        b.configure(command=shortcutButtonButtonCallback)
-        b.bind('<3>',deleteButtonCallback)
-    #@nonl
+        b = sc.createIconButton(
+            text='shortcut', #'shortcut-button',
+            # Do *not* call this just shortcut: it cause problems in k.registerCommand.
+            command=shortcutButtonButtonCallback,
+            shortcut=None,
+            statusLine='Create a shortcut button',
+            bg='LightSteelBlue1')
     #@-node:ekr.20060601153526:createShortcutButtonButton
     #@+node:ekr.20060601151845.10:createShortcutButton
     def createShortcutButton (self,sc,c):
         
         '''Create a button which selects the present position (when the button was created).'''
         p = c.currentPosition() ; h = p.headString()
-        buttonText = sc.getButtonText(h)
-        statusLine = "Shortcut %s" % h
         
-        b = sc.createIconButton(
-            text=buttonText,statusLine=statusLine,bg='LightSteelBlue1')
-    
-        def deleteButtonCallback(event=None,sc=sc,b=b):
-            sc.deleteButton(b)
-            
         def shortcutButtonCallback (event=None,c=c,p=p):
             c.beginUpdate()
             try:
                 c.selectPosition(p)
             finally:
                 c.endUpdate()
-    
-        b.configure(command=shortcutButtonCallback)
-        b.bind('<3>',deleteButtonCallback)
-    #@nonl
+        
+        b = sc.createIconButton(
+            text=h,
+            command=shortcutButtonCallback,
+            shortcut=None,
+            statusLine="Shortcut %s" % h,
+            bg='LightSteelBlue1')
     #@-node:ekr.20060601151845.10:createShortcutButton
     #@-others
 #@nonl
