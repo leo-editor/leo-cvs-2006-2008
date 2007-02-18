7398503dd1d936fc5de84594e9c6f5a5f8a07406
Edward K Ream <edreamleo@gmail.com>
2007-02-18 18:17:02 +0000

    Moved minibuffer label code from tk subclass to leoKeys base class. (The code is now completely gui-independent).

--- leo/src/leoKeys.py
+++ leo/src/leoKeys.py
@@ -3190,6 +3190,150 @@ class keyHandlerClass:
             return k.masterCommand(event,func,stroke,commandName)
     #@-node:ekr.20061031131434.155:masterMenuHandler
     #@-node:ekr.20061031131434.145:Master event handlers (keyHandler)
+    #@+node:ekr.20061031170011.3:Minibuffer (Tk keys)
+    # These may be overridden, but this code is now gui-independent.
+    #@nonl
+    #@+node:ekr.20061031170011.5:getLabel
+    def getLabel (self,ignorePrompt=False):
+        
+        k = self ; w = self.widget
+        if not w: return ''
+        
+        if self.useTextWidget:
+            w.update_idletasks()
+            s = w.getAllText()
+        else:
+            s = k.svar and k.svar.get()
+    
+        if ignorePrompt:
+            return s[len(k.mb_prefix):]
+        else:
+            return s or ''
+    #@-node:ekr.20061031170011.5:getLabel
+    #@+node:ekr.20061031170011.6:protectLabel
+    def protectLabel (self):
+        
+        k = self ; w = self.widget
+        if not w: return
+    
+        if self.useTextWidget:
+            w.update_idletasks()
+            k.mb_prefix = w.getAllText()
+        else:
+            if k.svar:
+                k.mb_prefix = k.svar.get()
+    #@-node:ekr.20061031170011.6:protectLabel
+    #@+node:ekr.20061031170011.7:resetLabel
+    def resetLabel (self):
+        
+        k = self
+        k.setLabelGrey('')
+        k.mb_prefix = ''
+    #@-node:ekr.20061031170011.7:resetLabel
+    #@+node:ekr.20061031170011.8:setLabel
+    def setLabel (self,s,protect=False):
+    
+        k = self ; c = k.c ; w = self.widget
+        if not w: return
+        trace = self.trace_minibuffer and not g.app.unitTesting
+    
+        trace and g.trace(repr(s),g.callers())
+    
+        if self.useTextWidget:
+            w.delete(0,'end')
+            w.insert(0,s)
+            c.masterFocusHandler() # Restore to the previously requested focus.
+        else:
+            if k.svar: k.svar.set(s)
+    
+        if protect:
+            k.mb_prefix = s
+    #@-node:ekr.20061031170011.8:setLabel
+    #@+node:ekr.20061031170011.9:extendLabel
+    def extendLabel(self,s,select=False,protect=False):
+        
+        k = self ; c = k.c ; w = self.widget
+        if not w: return
+        trace = self.trace_minibuffer and not g.app.unitTesting
+        
+        trace and g.trace(repr(s))
+        if not s: return
+    
+        if self.useTextWidget:
+            c.widgetWantsFocusNow(w)
+            w.insert('end',s)
+            if select:
+                i,j = k.getEditableTextRange()
+                w.setSelectionRange(i,j,insert=j)
+            if protect:
+                k.protectLabel()
+    #@-node:ekr.20061031170011.9:extendLabel
+    #@+node:ekr.20061031170011.10:setLabelBlue
+    def setLabelBlue (self,label=None,protect=False):
+        
+        k = self ; w = k.widget
+        if not w: return
+        
+        w.configure(background='lightblue')
+    
+        if label is not None:
+            k.setLabel(label,protect)
+    #@-node:ekr.20061031170011.10:setLabelBlue
+    #@+node:ekr.20061031170011.11:setLabelGrey
+    def setLabelGrey (self,label=None):
+    
+        k = self ; w = self.widget
+        if not w: return
+        
+        w.configure(background='lightgrey')
+        if label is not None:
+            k.setLabel(label)
+    
+    setLabelGray = setLabelGrey
+    #@-node:ekr.20061031170011.11:setLabelGrey
+    #@+node:ekr.20061031170011.12:updateLabel
+    def updateLabel (self,event):
+    
+        '''Mimic what would happen with the keyboard and a Text editor
+        instead of plain accumalation.'''
+        
+        k = self ; c = k.c ; w = self.widget
+        ch = (event and event.char) or ''
+        keysym = (event and event.keysym) or ''
+        trace = self.trace_minibuffer and not g.app.unitTesting
+    
+        trace and g.trace('ch',ch,'keysym',keysym,'k.stroke',k.stroke)
+        
+        if ch and ch not in ('\n','\r'):
+            if self.useTextWidget:
+                c.widgetWantsFocusNow(w)
+                i,j = w.getSelectionRange()
+                if i != j:
+                    w.delete(i,j)
+                if ch == '\b':
+                    s = w.getAllText()
+                    if len(s) > len(k.mb_prefix):
+                        w.delete(i-1)
+                else:
+                    w.insert('insert',ch)
+                # g.trace(k.mb_prefix)       
+            else:
+                # Just add the character.
+                k.setLabel(k.getLabel() + ch)
+    #@-node:ekr.20061031170011.12:updateLabel
+    #@+node:ekr.20061031170011.13:getEditableTextRange
+    def getEditableTextRange (self):
+        
+        k = self ; w = self.widget
+        s = w.getAllText()
+        # g.trace(len(s),repr(s))
+    
+        i = len(k.mb_prefix)
+        j = len(s)
+        return i,j
+    #@nonl
+    #@-node:ekr.20061031170011.13:getEditableTextRange
+    #@-node:ekr.20061031170011.3:Minibuffer (Tk keys)
     #@+node:ekr.20061031131434.156:Modes
     #@+node:ekr.20061031131434.157:badMode
     def badMode(self,modeName):
