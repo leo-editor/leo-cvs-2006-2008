ecdaf76afc6f90f357d1ce30781ec7bc40d36b3c
Edward K Ream <edreamleo@gmail.com>
2007-03-03 02:09:50 +0000

    Moved defineSpecialKeys into base keyHandlerClass.

--- leo/src/leoKeys.py
+++ leo/src/leoKeys.py
@@ -1783,16 +1783,61 @@ class keyHandlerClass:
     # KP_0,KP_1,KP_2,KP_3,KP_4,KP_5,KP_6,KP_7,KP_8,KP_9
     #@-at
     #@-node:ekr.20070123143428:k.defineTkNames
-    #@+node:ekr.20070123090310:k.defineSpecialKeys
+    #@+node:ekr.20070123085931:k.defineSpecialKeys
     def defineSpecialKeys (self):
         
-        # Should be defined in subclasses, but this will suffice.
         k = self
-    
-        k.settingsNameDict = {}
-        k.guiBindNamesDict = {}
+        
+        # These are defined at http://tcl.activestate.com/man/tcl8.4/TkCmd/keysyms.htm.
+        # Important: only the inverse dict is actually used in the new key binding scheme.
+        # Tk may return the *values* of this dict in event.keysym fields.
+        # Leo will warn if it gets a event whose keysym not in values of this table.
+        k.guiBindNamesDict = {
+            "&" : "ampersand",
+            "^" : "asciicircum",
+            "~" : "asciitilde",
+            "*" : "asterisk",
+            "@" : "at",
+            "\\": "backslash",
+            "|" : "bar",
+            "{" : "braceleft",
+            "}" : "braceright",
+            "[" : "bracketleft",
+            "]" : "bracketright",
+            ":" : "colon",      # removed from code.
+            "," : "comma",
+            "$" : "dollar",
+            "=" : "equal",
+            "!" : "exclam",     # removed from code.
+            ">" : "greater",
+            "<" : "less",
+            "-" : "minus",
+            "#" : "numbersign",
+            '"' : "quotedbl",
+            "'" : "quoteright",
+            "(" : "parenleft",
+            ")" : "parenright", # removed from code.
+            "%" : "percent",
+            "." : "period",     # removed from code.
+            "+" : "plus",
+            "?" : "question",
+            "`" : "quoteleft",
+            ";" : "semicolon",
+            "/" : "slash",
+            " " : "space",      # removed from code.
+            "_" : "underscore",
+        }
+        
+        # No translation.
+        for s in k.tkNamesList:
+            k.guiBindNamesDict[s] = s
+            
+        # Create the inverse dict.
         k.guiBindNamesInverseDict = {}
-    #@-node:ekr.20070123090310:k.defineSpecialKeys
+        for key in k.guiBindNamesDict.keys():
+            k.guiBindNamesInverseDict [k.guiBindNamesDict.get(key)] = key
+        
+    #@-node:ekr.20070123085931:k.defineSpecialKeys
     #@+node:ekr.20061101071425:oops
     def oops (self):
         
--- leo/src/leoTkinterKeys.py
+++ leo/src/leoTkinterKeys.py
@@ -30,61 +30,6 @@ class tkinterKeyHandlerClass (leoKeys.keyHandlerClass):
         else:
             self.svar = None
     #@-node:ekr.20061031170011.1:createTkIvars
-    #@+node:ekr.20070123085931: tkKeys.defineSpecialKeys
-    def defineSpecialKeys (self):
-        
-        k = self
-        
-        # These are defined at http://tcl.activestate.com/man/tcl8.4/TkCmd/keysyms.htm.
-        # Important: only the inverse dict is actually used in the new key binding scheme.
-        # Tk may return the *values* of this dict in event.keysym fields.
-        # Leo will warn if it gets a event whose keysym not in values of this table.
-        k.guiBindNamesDict = {
-            "&" : "ampersand",
-            "^" : "asciicircum",
-            "~" : "asciitilde",
-            "*" : "asterisk",
-            "@" : "at",
-            "\\": "backslash",
-            "|" : "bar",
-            "{" : "braceleft",
-            "}" : "braceright",
-            "[" : "bracketleft",
-            "]" : "bracketright",
-            ":" : "colon",      # removed from code.
-            "," : "comma",
-            "$" : "dollar",
-            "=" : "equal",
-            "!" : "exclam",     # removed from code.
-            ">" : "greater",
-            "<" : "less",
-            "-" : "minus",
-            "#" : "numbersign",
-            '"' : "quotedbl",
-            "'" : "quoteright",
-            "(" : "parenleft",
-            ")" : "parenright", # removed from code.
-            "%" : "percent",
-            "." : "period",     # removed from code.
-            "+" : "plus",
-            "?" : "question",
-            "`" : "quoteleft",
-            ";" : "semicolon",
-            "/" : "slash",
-            " " : "space",      # removed from code.
-            "_" : "underscore",
-        }
-        
-        # No translation.
-        for s in k.tkNamesList:
-            k.guiBindNamesDict[s] = s
-            
-        # Create the inverse dict.
-        k.guiBindNamesInverseDict = {}
-        for key in k.guiBindNamesDict.keys():
-            k.guiBindNamesInverseDict [k.guiBindNamesDict.get(key)] = key
-        
-    #@-node:ekr.20070123085931: tkKeys.defineSpecialKeys
     #@-others
 #@nonl
 #@-node:ekr.20031218072017.4099:@thin leoTkinterKeys.py
