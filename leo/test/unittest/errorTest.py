912d5a8d690cbda6a26fa623fa5b9e31651412eb
Edward K Ream <edreamleo@gmail.com>
2007-06-27 14:53:32 +0000

    Synch.

--- leo/test/unittest/errorTest.py
+++ leo/test/unittest/errorTest.py
@@ -1,5 +1,5 @@
 #@+leo-ver=4-thin
-#@+node:ekr.20070626094543.776:@thin ../test/unittest/errorTest.py
+#@+node:ekr.20070627082044.808:@thin ../test/unittest/errorTest.py
 # A file that contains functions with errors in them.
 # This is used to test error reporting in scripts
 
@@ -11,5 +11,5 @@ def testIndexError():
     a = []
     b = a[2]
 #@nonl
-#@-node:ekr.20070626094543.776:@thin ../test/unittest/errorTest.py
+#@-node:ekr.20070627082044.808:@thin ../test/unittest/errorTest.py
 #@-leo
