d117c0e8071cea29122b975bafb7ec7e20ac8ca8
Edward K Ream <edreamleo@gmail.com>
2007-07-02 14:21:02 +0000

    Updated version number to 4.4.3.1.

--- leo/src/leoCommands.py
+++ leo/src/leoCommands.py
@@ -307,7 +307,7 @@ class baseCommands:
     #@+node:ekr.20040629121554.1:getSignOnLine (Contains hard-coded version info)
     def getSignOnLine (self):
         c = self
-        return "Leo 4.4.3 final, build %s, June 26, 2007" % c.getBuildNumber()
+        return "Leo 4.4.3.1 final, build %s, July 2, 2007" % c.getBuildNumber()
     #@-node:ekr.20040629121554.1:getSignOnLine (Contains hard-coded version info)
     #@+node:ekr.20040629121554.2:initVersion
     def initVersion (self):
