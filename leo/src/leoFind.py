1000dd04c7295abf200c63e74120639224afb5b5
Edward K Ream <edreamleo@gmail.com>
2007-09-07 17:29:47 +0000

    Removed some spurious clones from the tree.

--- leo/src/leoFind.py
+++ leo/src/leoFind.py
@@ -1347,7 +1347,6 @@ class findTab (leoFind):
     '''An adapter class that implements Leo's Find tab.'''
 
     #@    @+others
-    #@+node:ekr.20051020120306.10:Birth & death
     #@+node:ekr.20051020120306.11:__init__ & initGui
     def __init__(self,c,parentFrame):
 
@@ -1372,33 +1371,6 @@ class findTab (leoFind):
         self.createBindings()
         self.init(c) # New in 4.3: init only once.
     #@-node:ekr.20051020120306.11:__init__ & initGui
-    #@+node:ekr.20061212092124:Defined in subclasses
-    def createBindings (self):
-        self.oops()
-
-    def createFrame (self,parent):
-        __pychecker__ = '--no-argsused'
-        self.oops()
-
-    def getOption (self,ivar):
-        self.oops()
-
-    def init (self,c):
-        self.oops()
-
-    def initGui (self):
-        pass # Optional method.
-
-    def setOption (self,ivar,val):
-        __pychecker__ = '--no-argsused'
-        self.oops()
-
-    def toggleOption (self,ivar):
-        self.oops()
-
-    # self.oops is defined in the leoFind class.
-    #@-node:ekr.20061212092124:Defined in subclasses
-    #@-node:ekr.20051020120306.10:Birth & death
     #@+node:ekr.20060221074900:Callbacks
     #@+node:ekr.20060221074900.1:findButtonCallback
     def findButtonCallback(self,event=None):
@@ -1472,30 +1444,6 @@ class findTab (leoFind):
         self.reverse = not self.reverse
     #@-node:ekr.20051024192642.2:findNext/PrefCommand
     #@-node:ekr.20051024192602: Top level
-    #@+node:ekr.20051020120306.11:__init__ & initGui
-    def __init__(self,c,parentFrame):
-
-        # g.trace('findTab',c)
-
-        # Init the base class...
-        leoFind.__init__(self,c,title='Find Tab')
-
-        self.c = c
-        self.parentFrame = parentFrame
-        self.frame = self.outerFrame = self.top = None
-
-        self.optionsOnly = c.config.getBool('show_only_find_tab_options')
-
-        # These are created later.
-        self.find_ctrl = None
-        self.change_ctrl = None 
-        self.outerScrolledFrame = None
-
-        self.initGui()
-        self.createFrame(parentFrame)
-        self.createBindings()
-        self.init(c) # New in 4.3: init only once.
-    #@-node:ekr.20051020120306.11:__init__ & initGui
     #@+node:ekr.20061212092124:Defined in subclasses
     def createBindings (self):
         self.oops()
