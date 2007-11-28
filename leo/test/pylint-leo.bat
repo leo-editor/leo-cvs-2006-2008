2d20a1494a11cadf6321ac6d197a10883d624271
Edward K Ream <edreamleo@gmail.com>
2007-11-28 00:35:43 +0000

    Synch.

--- leo/test/pylint-leo.bat
+++ leo/test/pylint-leo.bat
@@ -2,7 +2,7 @@ echo off
 rem ekr pylint-leo.bat
 rem WW0212Access to a protected member of a client class
 rem W0401 Wildcard import (pychecker)
-rem W0402 Uses of a deprecated module (string in leoAtFile)
+rem W0402 Uses of a deprecated module (like string)
 rem W0613 Unused argument (sometimes used for debugging)
 rem W0621 Redefining <name> from outer scope: especially __pychecker__
 
@@ -14,6 +14,6 @@ echo on
 
 cd c:\prog\pylint-0.13.2\bin
 
-call pylint.bat c:\prog\tigris-cvs\leo\src\leoImport.py --disable-msg= --rcfile=c:\prog\tigris-cvs\leo\test\pylint-leo-rc.txt
+call pylint.bat c:\prog\tigris-cvs\leo\src\leoImport.py --disable-msg=W0402 --rcfile=c:\prog\tigris-cvs\leo\test\pylint-leo-rc.txt
 
 pause
