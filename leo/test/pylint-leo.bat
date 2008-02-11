62c3b6fe7ac72d3c0ef517c814f043c01a0082ff
Edward K Ream <edreamleo@gmail.com>
2008-02-11 23:27:05 +0000

    Synch.

--- leo/test/pylint-leo.bat
+++ leo/test/pylint-leo.bat
@@ -11,7 +11,6 @@ REM *** leoEditCommands.py: Dangerous: E1101
 REM *** leoGlobals.py: Dangerous: E0602,E1101.
 REM *** leoTkinterFrame.py: Harmless: W0221: mismatch between Tk.Text methods and overridden methods.
 rem call pylint.bat c:\prog\tigris-cvs\leo\src\leoAtFile.py        --rcfile=c:\prog\tigris-cvs\leo\test\pylint-leo-rc.txt
-rem call pylint.bat c:\prog\tigris-cvs\leo\src\leoAtFile.py        --rcfile=c:\prog\tigris-cvs\leo\test\pylint-leo-rc.txt
 rem call pylint.bat c:\prog\tigris-cvs\leo\src\leoChapters.py      --rcfile=c:\prog\tigris-cvs\leo\test\pylint-leo-rc.txt
 rem call pylint.bat c:\prog\tigris-cvs\leo\src\leoCommands.py      --rcfile=c:\prog\tigris-cvs\leo\test\pylint-leo-rc.txt
 rem call pylint.bat c:\prog\tigris-cvs\leo\src\leoEditCommands.py  --rcfile=c:\prog\tigris-cvs\leo\test\pylint-leo-rc.txt
@@ -34,10 +33,52 @@ rem call pylint.bat c:\prog\tigris-cvs\leo\src\leoTkinterKeys.py   --rcfile=c:\p
 rem call pylint.bat c:\prog\tigris-cvs\leo\src\leoTkinterMenu.py   --rcfile=c:\prog\tigris-cvs\leo\test\pylint-leo-rc.txt
 rem call pylint.bat c:\prog\tigris-cvs\leo\src\leoTkinterTree.py   --rcfile=c:\prog\tigris-cvs\leo\test\pylint-leo-rc.txt
 
+REM echo on
 
-echo on
-
-cd c:\prog\pylint-0.13.2\bin
+echo leoAtFile.py
+call pylint.bat c:\prog\tigris-cvs\leo\src\leoAtFile.py        --rcfile=c:\prog\tigris-cvs\leo\test\pylint-leo-rc.txt
+echo leoChapters.py
+call pylint.bat c:\prog\tigris-cvs\leo\src\leoChapters.py      --rcfile=c:\prog\tigris-cvs\leo\test\pylint-leo-rc.txt
+echo leoCommands.py
+call pylint.bat c:\prog\tigris-cvs\leo\src\leoCommands.py      --rcfile=c:\prog\tigris-cvs\leo\test\pylint-leo-rc.txt
+echo leoEditCommands.py
+call pylint.bat c:\prog\tigris-cvs\leo\src\leoEditCommands.py  --rcfile=c:\prog\tigris-cvs\leo\test\pylint-leo-rc.txt
+echo leoFileCommands.py
+call pylint.bat c:\prog\tigris-cvs\leo\src\leoFileCommands.py  --rcfile=c:\prog\tigris-cvs\leo\test\pylint-leo-rc.txt
+echo leoFind.py
+call pylint.bat c:\prog\tigris-cvs\leo\src\leoFind.py          --rcfile=c:\prog\tigris-cvs\leo\test\pylint-leo-rc.txt
+echo leoFrame.py
+call pylint.bat c:\prog\tigris-cvs\leo\src\leoFrame.py         --rcfile=c:\prog\tigris-cvs\leo\test\pylint-leo-rc.txt
+echo leoGlobals.py
+call pylint.bat c:\prog\tigris-cvs\leo\src\leoGlobals.py       --rcfile=c:\prog\tigris-cvs\leo\test\pylint-leo-rc.txt
+echo leoGui.py
+call pylint.bat c:\prog\tigris-cvs\leo\src\leoGui.py           --rcfile=c:\prog\tigris-cvs\leo\test\pylint-leo-rc.txt
+echo leoImport.py
+call pylint.bat c:\prog\tigris-cvs\leo\src\leoImport.py        --rcfile=c:\prog\tigris-cvs\leo\test\pylint-leo-rc.txt
+echo leoMenu.py
+call pylint.bat c:\prog\tigris-cvs\leo\src\leoMenu.py          --rcfile=c:\prog\tigris-cvs\leo\test\pylint-leo-rc.txt
+echo leoNodes.py
+call pylint.bat c:\prog\tigris-cvs\leo\src\leoNodes.py         --rcfile=c:\prog\tigris-cvs\leo\test\pylint-leo-rc.txt
+echo leoPlugins.py
+call pylint.bat c:\prog\tigris-cvs\leo\src\leoPlugins.py       --rcfile=c:\prog\tigris-cvs\leo\test\pylint-leo-rc.txt
+echo leoTangle.py
+call pylint.bat c:\prog\tigris-cvs\leo\src\leoTangle.py        --rcfile=c:\prog\tigris-cvs\leo\test\pylint-leo-rc.txt
+echo leoUndo.py
+call pylint.bat c:\prog\tigris-cvs\leo\src\leoUndo.py          --disable-msg=W0102 --rcfile=c:\prog\tigris-cvs\leo\test\pylint-leo-rc.txt
+echo leoTkinterDialog.py
+call pylint.bat c:\prog\tigris-cvs\leo\src\leoTkinterDialog.py --rcfile=c:\prog\tigris-cvs\leo\test\pylint-leo-rc.txt
+echo leoTkinterFind.py
+call pylint.bat c:\prog\tigris-cvs\leo\src\leoTkinterFind.py   --rcfile=c:\prog\tigris-cvs\leo\test\pylint-leo-rc.txt
+echo leoTkinterGui.py
+call pylint.bat c:\prog\tigris-cvs\leo\src\leoTkinterGui.py    --rcfile=c:\prog\tigris-cvs\leo\test\pylint-leo-rc.txt
+echo leoTkinterFrame.py
+call pylint.bat c:\prog\tigris-cvs\leo\src\leoTkinterFrame.py  --disable-msg=W0221 --rcfile=c:\prog\tigris-cvs\leo\test\pylint-leo-rc.txt
+echo leoTkinterKeys.py
+call pylint.bat c:\prog\tigris-cvs\leo\src\leoTkinterKeys.py   --rcfile=c:\prog\tigris-cvs\leo\test\pylint-leo-rc.txt
+echo leoTkinterMenu.py
+call pylint.bat c:\prog\tigris-cvs\leo\src\leoTkinterMenu.py   --rcfile=c:\prog\tigris-cvs\leo\test\pylint-leo-rc.txt
+echo leoTkinterTree.py
+call pylint.bat c:\prog\tigris-cvs\leo\src\leoTkinterTree.py   --rcfile=c:\prog\tigris-cvs\leo\test\pylint-leo-rc.txt
 
 echo "*****done*****"
 
